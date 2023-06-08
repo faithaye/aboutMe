1b87adbaf725bdfef958c4a6df7104fc7ca18cc1
faith <faith102005@gmail.com>
2023-06-08 13:32:47 -0500

    about me project

--- aboutMe/ContentView.swift
+++ aboutMe/ContentView.swift
@@ -19,7 +19,7 @@ struct ContentView: View {
             VStack {
                 Text("🤣")
                 Button("Me"){
-                    Ye
+                    List ;Ye
                 }
                 .font(.title)
                 .buttonStyle(.borderedProminent)
